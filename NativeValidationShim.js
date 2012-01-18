548f5187fd70c943992331a46d998bf1943c66e4
Michael Cordingley <Michael.Cordingley@gmail.com>
2012-01-18 11:16:51 -0500

    Added event handlers to set up validations on page submit for each form and the validation loop of all elements in each form.

--- NativeValidationShim.js
+++ NativeValidationShim.js
@@ -1,8 +1,42 @@
 (function(root, undefined) {
+	//Set-up settings and other variables.
 	var lib = {};
 	
 	lib.version = '0.0';
 	lib.settings = {};
+	
+	//Create all of the validation rules.
+	
+	//Cache any pre-existing load event.
+	var old_load = document.onload || function() {};
+	
+	//Wait until the page has finished loading to attach to all page forms.
+	document.onload = function() {
+		//Attach to every form on the page.
+		for (var i = 0; i < document.forms.length; i++) {
+			var form = document.forms[i];
+			
+			//Cache old callback.
+			var old_submit = form.onsubmit || function() {};
+			
+			//Set our new one.
+			form.onsubmit = function() {
+				//Test all elements on the form.
+				for (var j = 0; j < form.elements.length; j++) {
+					var element = form.elements[j];
+					
+					
+				}
+			
+				//Run the old callback after all is clear.
+				old_submit();
+			};
+		}
+		
+		//Run a pre-existing load event.
+		old_load();
+	};
 
+	//Expose the library to the root namespace.
 	root['NativeValidationShim'] = lib;
 }(this));
